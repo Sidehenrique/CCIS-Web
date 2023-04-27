ce82bc95c5599eca776e3d359ea9052321e7d3c8
SideHenrique <henriquesilver585@gmail.com>
2023-04-27 11:27:42 -0300

    # Checkpoint 19 1 - Pagina Home

--- ccis/static/ccis/script.js
+++ ccis/static/ccis/script.js
@@ -1,8 +1,3 @@
-$(document).ready(function(){
-    $('.toast').toast('show');
-});
-
-
 function filterTable() {
   var input, filter, table, tr, td, i, txtValue;
   input = document.getElementById("inputUser");
@@ -23,3 +18,23 @@ function filterTable() {
   }
 }
 
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(function () {
+    'use strict'
+
+// Fetch all the forms we want to apply custom Bootstrap validation styles to
+var forms = document.querySelectorAll('.needs-validation')
+
+// Loop over them and prevent submission
+Array.prototype.slice.call(forms)
+.forEach(function (form) {
+form.addEventListener('submit', function (event) {
+if (!form.checkValidity()) {
+event.preventDefault()
+event.stopPropagation()
+}
+
+form.classList.add('was-validated')
+}, false)
+})
+})()
