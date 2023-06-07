9e2a1156e31437d5ac5c710a054689409e6737c7
SideHenrique <henriquesilver585@gmail.com>
2023-06-07 15:38:50 -0300

    Checkpoint 25 1 - Correção de fonts, 2 - Limpeza do style, 3 - correção do documentos, 4 - ajustes no usuario.

--- core/settings.py
+++ core/settings.py
@@ -129,9 +129,9 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DATA_UPLOAD_MAX_MEMORY_SIZE = 5242880 # 5 megabytes
 
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, 'staticfiles')
-# ]
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'staticfiles')
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
