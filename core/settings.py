4f8ee662fff8cdd084be0549057eac4d340a18a5
ACalorio <arthur.calorio@sicoobbrasilia.com.br>
2023-10-26 09:03:29 -0300

    atualiazação dos links

--- core/settings.py
+++ core/settings.py
@@ -82,7 +82,7 @@ WSGI_APPLICATION = 'core.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'dados_rh',
+        'NAME': 'teste2',
         'USER': 'root',
         'PASSWORD': 'Sicoob4155@@',
         'HOST': '10.6.32.24',
