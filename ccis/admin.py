67e31fc32b370fb80cec7af1299e0449f8c521d9
SideHenrique <henriquesilver585@gmail.com>
2023-10-26 10:26:52 -0300

    Loginca de botoes

--- ccis/admin.py
+++ ccis/admin.py
@@ -1,9 +1,8 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin, GroupAdmin
 from .models import dadosPessoais, dependentes, enderecoContato, escolaridade, certificacao, profissional, \
-    dadosBancarios, outros, CustomGroupInfo
+    dadosBancarios, outros, CustomGroupInfo, CustomButtonsSector
 from django.contrib.auth.models import User, Group
-
 
 # INSERÇÃO DE TABELAS NO CADASTRO DE USUARIO NO ADMIN ------------------------------------------------------------------
 class dadosPessoaisInline(admin.StackedInline):
@@ -11,38 +10,36 @@ class dadosPessoaisInline(admin.StackedInline):
     fields = ['nomeCompleto', 'sexo', 'cpf']
     can_delete = False
 
-
 class enderecoContatoInline(admin.StackedInline):
     model = enderecoContato
     fields = ['emailCorporativo']
     can_delete = False
-
 
 class profissionalInline(admin.StackedInline):
     model = profissional
     fields = ['cargo', 'area', 'situacao']
     can_delete = False
 
+@admin.register(CustomButtonsSector)
+class CustomButtonsSectorAdmin(admin.ModelAdmin):
+    list_display = ('group', 'text1', 'text2', 'cor', 'link', 'icon', 'permissao')
+    list_filter = ('cor', 'permissao')
+    search_fields = ('group__name', 'text1', 'text2')
 
-class CustomGroupInline(admin.TabularInline):  # Use TabularInline ou StackedInline, dependendo da aparência desejada
+class CustomGroupInline(admin.TabularInline):
     model = CustomGroupInfo
     can_delete = False
-    verbose_name = 'Custom Group'  # Nome da seção no admin
+    verbose_name = 'Custom Group'
     verbose_name_plural = 'Custom Groups'
 
-
 class CustomUserAdmin(UserAdmin):
-    inlines = (dadosPessoaisInline, enderecoContatoInline, profissionalInline)
-
+    inlines = (dadosPessoaisInline, enderecoContatoInline, profissionalInline, )
 
 class CustomGroupAdmin(GroupAdmin):
     inlines = [CustomGroupInline]
 
-
 # Registre o modelo DadosPessoais com o admin personalizado
 admin.site.unregister(User)
 admin.site.register(User, CustomUserAdmin)
-
-
 admin.site.unregister(Group)
 admin.site.register(Group, CustomGroupAdmin)
