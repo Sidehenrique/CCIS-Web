310c5894370e0c6d808023cc510de64b4426f1d7
SideHenrique <henriquesilver585@gmail.com>
2023-10-26 13:38:20 -0300

    Loginca de botoes

--- ccis/admin.py
+++ ccis/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin, GroupAdmin
 from .models import dadosPessoais, dependentes, enderecoContato, escolaridade, certificacao, profissional, \
-    dadosBancarios, outros, CustomGroupInfo, CustomButtonsSector
+    dadosBancarios, outros, CustomGroupInfo, SectorButtons
 from django.contrib.auth.models import User, Group
+
 
 # INSERÇÃO DE TABELAS NO CADASTRO DE USUARIO NO ADMIN ------------------------------------------------------------------
 class dadosPessoaisInline(admin.StackedInline):
@@ -10,21 +11,25 @@ class dadosPessoaisInline(admin.StackedInline):
     fields = ['nomeCompleto', 'sexo', 'cpf']
     can_delete = False
 
+
 class enderecoContatoInline(admin.StackedInline):
     model = enderecoContato
     fields = ['emailCorporativo']
     can_delete = False
+
 
 class profissionalInline(admin.StackedInline):
     model = profissional
     fields = ['cargo', 'area', 'situacao']
     can_delete = False
 
-@admin.register(CustomButtonsSector)
+
+@admin.register(SectorButtons)
 class CustomButtonsSectorAdmin(admin.ModelAdmin):
-    list_display = ('group', 'text1', 'text2', 'cor', 'link', 'icon', 'permissao')
+    list_display = ('group', 'text1', 'text2', 'cor', 'url', 'icon', 'permissao')
     list_filter = ('cor', 'permissao')
     search_fields = ('group__name', 'text1', 'text2')
+
 
 class CustomGroupInline(admin.TabularInline):
     model = CustomGroupInfo
@@ -32,11 +37,14 @@ class CustomGroupInline(admin.TabularInline):
     verbose_name = 'Custom Group'
     verbose_name_plural = 'Custom Groups'
 
+
 class CustomUserAdmin(UserAdmin):
-    inlines = (dadosPessoaisInline, enderecoContatoInline, profissionalInline, )
+    inlines = (dadosPessoaisInline, enderecoContatoInline, profissionalInline,)
+
 
 class CustomGroupAdmin(GroupAdmin):
     inlines = [CustomGroupInline]
+
 
 # Registre o modelo DadosPessoais com o admin personalizado
 admin.site.unregister(User)
