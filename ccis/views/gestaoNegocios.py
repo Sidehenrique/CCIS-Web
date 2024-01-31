e861ab0047097a9512d2f65ab110cb275855a46f
ACalorio <arthur.calorio@sicoobbrasilia.com.br>
2024-01-31 16:46:16 -0300

    Atualização de links

--- ccis/views/gestaoNegocios.py
+++ ccis/views/gestaoNegocios.py
@@ -8,393 +8,98 @@ from ..models import dadosPessoais
 @login_required(login_url="/login")
 def home(request):
 
-    user = request.user
-
-    log = request.user
-    log_id = request.user.id
-    logName = request.user.first_name
-    logLast = request.user.last_name
-    logFoto = dadosPessoais.objects.get(usuario=request.user).foto
-    is_superadmin = log.is_superuser
-
-    group_gestao = log.groups.filter(id=3).exists()
-
-    first_name = user.first_name
-    last_name = user.last_name
-
-    dados = dadosPessoais.objects.get(usuario=user)
-    groupControle = log.groups.filter(id=28).exists()
-
-    context = {
-        'log_id': log_id, 'logName': logName, 'logLast': logLast, 'logFoto': logFoto,
-        'dados': dados, 'username': user, 'first_name': first_name, 'groupControle':groupControle,
-        'last_name': last_name, 'group_gestao': group_gestao, 'is_superadmin': is_superadmin,
-    }
-
-    return render(request, 'gestaoNegocios/home.html', context)
+    return render(request, 'gestaoNegocios/home.html')
 
 
 @login_required(login_url="/login")
 def gestaoMetas(request):
-    # user = get_object_or_404(User, id=user_id)
 
-    user = request.user
-
-    log = request.user
-    log_id = request.user.id
-    logName = request.user.first_name
-    logLast = request.user.last_name
-    logFoto = dadosPessoais.objects.get(usuario=request.user).foto
-    is_superadmin = log.is_superuser
-
-    group_gestao = log.groups.filter(id=3).exists()
-    groupControle = log.groups.filter(id=28).exists()
-
-    first_name = user.first_name
-    last_name = user.last_name
-
-    dados = dadosPessoais.objects.get(usuario=user)
-
-    if request.method == 'GET':
-        context = {
-            'log_id': log_id, 'logName': logName, 'logLast': logLast, 'logFoto': logFoto,
-            'dados': dados, 'username': user, 'first_name': first_name, 'groupControle':groupControle,
-            'last_name': last_name, 'group_gestao': group_gestao, 'is_superadmin': is_superadmin,
-        }
-
-        return render(request, 'gestaoNegocios/gestaoMetas.html', context)
+    return render(request, 'gestaoNegocios/gestaoMetas.html')
     
 
 @login_required(login_url="/login")
 def diaria(request):
-    # user = get_object_or_404(User, id=user_id)
 
-    user = request.user
-
-    log = request.user
-    log_id = request.user.id
-    logName = request.user.first_name
-    logLast = request.user.last_name
-    logFoto = dadosPessoais.objects.get(usuario=request.user).foto
-    is_superadmin = log.is_superuser
-
-    group_gestao = log.groups.filter(id=3).exists()
-    groupControle = log.groups.filter(id=28).exists()
-
-    first_name = user.first_name
-    last_name = user.last_name
-
-    dados = dadosPessoais.objects.get(usuario=user)
-
-    if request.method == 'GET':
-        context = {
-            'log_id': log_id, 'logName': logName, 'logLast': logLast, 'logFoto': logFoto,
-            'dados': dados, 'username': user, 'first_name': first_name, 'groupControle':groupControle,
-            'last_name': last_name, 'group_gestao': group_gestao, 'is_superadmin': is_superadmin,
-        }
-
-        return render(request, 'gestaoNegocios/diaria.html', context)
+    return render(request, 'gestaoNegocios/diaria.html')
     
 
 @login_required(login_url="/login")
 def listasPropensos(request):
-    # user = get_object_or_404(User, id=user_id)
 
-    user = request.user
-
-    log = request.user
-    log_id = request.user.id
-    logName = request.user.first_name
-    logLast = request.user.last_name
-    logFoto = dadosPessoais.objects.get(usuario=request.user).foto
-    is_superadmin = log.is_superuser
-
-    group_gestao = log.groups.filter(id=3).exists()
-    groupControle = log.groups.filter(id=28).exists()
-
-    first_name = user.first_name
-    last_name = user.last_name
-
-    dados = dadosPessoais.objects.get(usuario=user)
-
-    if request.method == 'GET':
-        context = {
-            'log_id': log_id, 'logName': logName, 'logLast': logLast, 'logFoto': logFoto,
-            'dados': dados, 'username': user, 'first_name': first_name, 'groupControle':groupControle,
-            'last_name': last_name, 'group_gestao': group_gestao, 'is_superadmin': is_superadmin,
-        }
-
-        return render(request, 'gestaoNegocios/listasPropensos.html', context)
+    return render(request, 'gestaoNegocios/listasPropensos.html')
     
 
 @login_required(login_url="/login")
 def planoMetas(request):
-    # user = get_object_or_404(User, id=user_id)
 
-    user = request.user
-
-    group_gestao = user.groups.filter(id=3).exists()
-    groupControle = user.groups.filter(id=28).exists()
-
-    first_name = user.first_name
-    last_name = user.last_name
-
-    dados = dadosPessoais.objects.get(usuario=user)
-
-    if request.method == 'GET':
-        context = {
-            'dados': dados, 'username': user, 'first_name': first_name, 'groupControle':groupControle,
-            'last_name': last_name, 'group_gestao': group_gestao
-        }
-
-        return render(request, 'gestaoNegocios/planoMetas.html', context)
+    return render(request, 'gestaoNegocios/planoMetas.html')
     
 
 @login_required(login_url="/login")
 def pronampe(request):
-    # user = get_object_or_404(User, id=user_id)
-
-    user = request.user
-
-    group_gestao = user.groups.filter(id=3).exists()
-    groupControle = user.groups.filter(id=28).exists()
-
-    first_name = user.first_name
-    last_name = user.last_name
-
-    dados = dadosPessoais.objects.get(usuario=user)
-
-    if request.method == 'GET':
-        context = {
-            'dados': dados, 'username': user, 'first_name': first_name, 'groupControle':groupControle,
-            'last_name': last_name, 'group_gestao': group_gestao
-        }
-
-        return render(request, 'gestaoNegocios/pronampe.html', context)
+    return render(request, 'gestaoNegocios/pronampe.html')
     
 
 @login_required(login_url="/login")
 def relatorioVisitas(request):
-    # user = get_object_or_404(User, id=user_id)
-
-    user = request.user
-
-
-    group_gestao = user.groups.filter(id=3).exists()
-    groupControle = user.groups.filter(id=28).exists()
-
-    first_name = user.first_name
-    last_name = user.last_name
-
-    dados = dadosPessoais.objects.get(usuario=user)
-
-    if request.method == 'GET':
-        context = {
-            'dados': dados, 'username': user, 'first_name': first_name, 'groupControle':groupControle,
-            'last_name': last_name, 'group_gestao': group_gestao
-        }
-
-        return render(request, 'gestaoNegocios/relatorioVisitas.html', context)
+    return render(request, 'gestaoNegocios/relatorioVisitas.html')
 
 
 @login_required(login_url="/login")
 def lastro(request):
-    user = request.user
-
-    group_gestao = user.groups.filter(id=3).exists()
-
-    first_name = user.first_name
-    last_name = user.last_name
-
-    dados = dadosPessoais.objects.get(usuario=user)
-    groupControle = user.groups.filter(id=28).exists()
-
-    context = {
-        'dados': dados, 'username': user, 'first_name': first_name, 'groupControle': groupControle,
-        'last_name': last_name, 'group_gestao': group_gestao
-    }
-
-    return render(request, 'gestaoNegocios/lastro.html', context)
+    return render(request, 'gestaoNegocios/lastro.html')
 
 
 @login_required(login_url="/login")
 def credito(request):
-    user = request.user
 
-    group_gestao = user.groups.filter(id=3).exists()
-
-    first_name = user.first_name
-    last_name = user.last_name
-
-    dados = dadosPessoais.objects.get(usuario=user)
-    groupControle = user.groups.filter(id=28).exists()
-
-    context = {
-        'dados': dados, 'username': user, 'first_name': first_name, 'groupControle': groupControle,
-        'last_name': last_name, 'group_gestao': group_gestao,
-    }
-
-    return render(request, 'gestaoNegocios/credito.html', context)
+    return render(request, 'gestaoNegocios/credito.html')
 
 
 @login_required(login_url="/login")
 def operacoes_credito(request):
-    user = request.user
-
-    group_gestao = user.groups.filter(id=3).exists()
-
-    first_name = user.first_name
-    last_name = user.last_name
-
-    dados = dadosPessoais.objects.get(usuario=user)
-    groupControle = user.groups.filter(id=28).exists()
-
-    context = {
-        'dados': dados, 'username': user, 'first_name': first_name, 'groupControle': groupControle,
-        'last_name': last_name, 'group_gestao': group_gestao,
-    }
-
-    return render(request, 'gestaoNegocios/operacoes_credito.html', context)
+    return render(request, 'gestaoNegocios/operacoes_credito.html')
 
 
 @login_required(login_url="/login")
 def credito_liberacao(request):
-    user = request.user
 
-    group_gestao = user.groups.filter(id=3).exists()
-
-    first_name = user.first_name
-    last_name = user.last_name
-
-    dados = dadosPessoais.objects.get(usuario=user)
-    groupControle = user.groups.filter(id=28).exists()
-
-    context = {
-        'dados': dados, 'username': user, 'first_name': first_name, 'groupControle': groupControle,
-        'last_name': last_name, 'group_gestao': group_gestao,
-    }
-
-    return render(request, 'gestaoNegocios/credito_liberacao.html', context)
+    return render(request, 'gestaoNegocios/credito_liberacao.html')
 
 
 @login_required(login_url="/login")
 def calendario(request):
-    user = request.user
 
-    group_gestao = user.groups.filter(id=3).exists()
-
-    first_name = user.first_name
-    last_name = user.last_name
-
-    dados = dadosPessoais.objects.get(usuario=user)
-    groupControle = user.groups.filter(id=28).exists()
-
-    context = {
-        'dados': dados, 'username': user, 'first_name': first_name, 'groupControle': groupControle,
-        'last_name': last_name, 'group_gestao': group_gestao,
-    }
-
-    return render(request, 'gestaoNegocios/calendario.html', context)
+    return render(request, 'gestaoNegocios/calendario.html')
 
 
 @login_required(login_url="/login")
 def gestaoCarteira(request):
-    user = request.user
 
-    group_gestao = user.groups.filter(id=3).exists()
-
-    first_name = user.first_name
-    last_name = user.last_name
-
-    dados = dadosPessoais.objects.get(usuario=user)
-    groupControle = user.groups.filter(id=28).exists()
-
-    context = {
-        'dados': dados, 'username': user, 'first_name': first_name, 'groupControle': groupControle,
-        'last_name': last_name, 'group_gestao': group_gestao,
-    }
-
-    return render(request, 'gestaoNegocios/gestaoCarteira.html', context)
+    return render(request, 'gestaoNegocios/gestaoCarteira.html')
 
 
 @login_required(login_url="/login")
 def carteiras(request):
-    user = request.user
 
-    group_gestao = user.groups.filter(id=3).exists()
-
-    first_name = user.first_name
-    last_name = user.last_name
-
-    dados = dadosPessoais.objects.get(usuario=user)
-    groupControle = user.groups.filter(id=28).exists()
-
-    context = {
-        'dados': dados, 'username': user, 'first_name': first_name, 'groupControle': groupControle,
-        'last_name': last_name, 'group_gestao': group_gestao,
-    }
-
-    return render(request, 'gestaoNegocios/carteiras.html', context)
+    return render(request, 'gestaoNegocios/carteiras.html')
 
 
 @login_required(login_url="/login")
 def capitalRecorrente(request):
-    user = request.user
 
-    group_gestao = user.groups.filter(id=3).exists()
-
-    first_name = user.first_name
-    last_name = user.last_name
-
-    dados = dadosPessoais.objects.get(usuario=user)
-    groupControle = user.groups.filter(id=28).exists()
-
-    context = {
-        'dados': dados, 'username': user, 'first_name': first_name, 'groupControle': groupControle,
-        'last_name': last_name, 'group_gestao': group_gestao,
-    }
-
-    return render(request, 'gestaoNegocios/capitalRecorrente.html', context)
+    return render(request, 'gestaoNegocios/resultadoCoop.html')
 
 
 @login_required(login_url="/login")
 def contasInativas(request):
-    user = request.user
 
-    group_gestao = user.groups.filter(id=3).exists()
-
-    first_name = user.first_name
-    last_name = user.last_name
-
-    dados = dadosPessoais.objects.get(usuario=user)
-    groupControle = user.groups.filter(id=28).exists()
-
-    context = {
-        'dados': dados, 'username': user, 'first_name': first_name, 'groupControle': groupControle,
-        'last_name': last_name, 'group_gestao': group_gestao,
-    }
-
-    return render(request, 'gestaoNegocios/contasInativas.html', context)
+    return render(request, 'gestaoNegocios/contasInativas.html')
 
 
 @login_required(login_url="/login")
-def margemContri(request):
-    user = request.user
-
-    group_gestao = user.groups.filter(id=3).exists()
-
-    first_name = user.first_name
-    last_name = user.last_name
-
-    dados = dadosPessoais.objects.get(usuario=user)
-    groupControle = user.groups.filter(id=28).exists()
-
-    context = {
-        'dados': dados, 'username': user, 'first_name': first_name, 'groupControle': groupControle,
-        'last_name': last_name, 'group_gestao': group_gestao,
-    }
-
-    return render(request, 'gestaoNegocios/margemContri.html', context)
+def resultadoCoop(request):
+    return render(request, 'gestaoNegocios/resultadoCoop.html')
 
 
 
